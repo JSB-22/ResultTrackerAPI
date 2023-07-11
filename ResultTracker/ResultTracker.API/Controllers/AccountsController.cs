4bc4ff4452547e23778518c7a609ceabd50ba131
JSB-22 <j.s.banyard@gmail.com>
2023-07-11 11:02:00 +0100

    Added Account Controller in API for list functionality later

---
+++ ResultTracker/ResultTracker.API/Controllers/AccountsController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResultTracker.API.Models.Dto;
+using ResultTracker.API.Repositories.Interfaces;
+using ResultTracker.API.Users;
+
+namespace ResultTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountRepository _repository;
+        private readonly IMapper _mapper;
+        public AccountsController(IAccountRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllAccounts()
+        {
+            var subjects = await _repository.GetAllAsync();
+            return Ok(subjects.Select(t => _mapper.Map<AccountDto>(t)));
+        }
+    }
+}
