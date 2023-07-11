7c2d5fa88df3578b6da908eb623690a790221626
JSB-22 <j.s.banyard@gmail.com>
2023-07-11 11:03:52 +0100

    Added Auth tag to GetAll in Accounts Controller API

--- ResultTracker/ResultTracker.API/Controllers/AccountsController.cs
+++ ResultTracker/ResultTracker.API/Controllers/AccountsController.cs
@@ -1,14 +1,17 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ResultTracker.API.Models.Dto;
 using ResultTracker.API.Repositories.Interfaces;
 using ResultTracker.API.Users;
+using System.Data;
 
 namespace ResultTracker.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Teacher, Admin")]
     public class AccountsController : ControllerBase
     {
         private readonly IAccountRepository _repository;
