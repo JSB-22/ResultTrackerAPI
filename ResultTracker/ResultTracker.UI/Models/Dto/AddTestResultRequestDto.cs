a4c21210e2b1252a378315faf3420ef88d944a50
JSB-22 <j.s.banyard@gmail.com>
2023-07-11 14:14:04 +0100

    Added validation tags for dropdown lists

--- ResultTracker/ResultTracker.UI/Models/Dto/AddTestResultRequestDto.cs
+++ ResultTracker/ResultTracker.UI/Models/Dto/AddTestResultRequestDto.cs
@@ -1,12 +1,18 @@
-﻿namespace ResultTracker.UI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResultTracker.UI.Models.Dto
 {
 	public class AddTestResultRequestDto
 	{
+		[StringLength(100)]
 		public string? Notes { get; set; }
+		[Required(ErrorMessage = "A grade is required")]
 		public int PercentageResult { get; set; }
+		[Required(ErrorMessage = "A Topic must be assigned")]
 		public Guid TopicId { get; set; }
+		[Required(ErrorMessage = "A Subject must be assigned")]
 		public Guid SubjectId { get; set; }
-
+		[Required(ErrorMessage = "A Student must be assigned")]
 		public string StudentId { get; set; } = "";
 	}
 }
--- ResultTracker/ResultTracker.UI/Models/Dto/AddTestResultViewModel.cs
+++ ResultTracker/ResultTracker.UI/Models/Dto/AddTestResultViewModel.cs
@@ -1,20 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace ResultTracker.UI.Models.Dto
 {
     public class AddTestResultViewModel
     {
 
-        // For Drop down list functionality:
-        public List<SelectListItem> TopicSelectList { get; set; }
-        public string SelectedTopicId { get; set; } = string.Empty;
-        public List<SelectListItem> SubjectSelectList { get; set; }
-        public string SelectedSubjectId { get; set; } = string.Empty; //Convert back to GUID before post. 
-        public List<SelectListItem> AccountSelectList { get; set; }
-        public string SelectedAccountId { get; set; } = string.Empty;
+		// For Drop down list functionality:
+		[Required(ErrorMessage = "A Topic must be assigned")]
+		public List<SelectListItem> TopicSelectList { get; set; }
+		[Required, Range(1, int.MaxValue, ErrorMessage = "Select a Topic")] // Range used so default option not usable. 
+		public string SelectedTopicId { get; set; } = string.Empty;
+		[Required(ErrorMessage = "A Subject must be assigned")]
+		public List<SelectListItem> SubjectSelectList { get; set; }
+		[Required, Range(1, int.MaxValue, ErrorMessage = "Select a Subject")]
+		public string SelectedSubjectId { get; set; } = string.Empty; //Convert back to GUID before post. 
+		[Required(ErrorMessage = "A Student must be assigned")]
+		public List<SelectListItem> AccountSelectList { get; set; }
+		[Required, Range(1, int.MaxValue, ErrorMessage = "Select a Student")]
+		public string SelectedAccountId { get; set; } = string.Empty;
 
-        // Properties not requiring drop down: 
-        public string? Notes { get; set; }
-        public int PercentageResult { get; set; }
-    }
+		// Properties not requiring drop down: 
+		[StringLength(100)]
+		public string? Notes { get; set; }
+		[Required(ErrorMessage = "A grade is required")]
+		public int PercentageResult { get; set; }
+	}
 }
