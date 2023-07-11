c1f2207e59587b2f749e4acfa8462eb5e67fdb1d
JSB-22 <j.s.banyard@gmail.com>
2023-07-11 11:26:26 +0100

    Foundation work of index page for TestResults

---
+++ ResultTracker/ResultTracker.UI/Controllers/TestResultsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResultTracker.UI.Models.Dto;
+using System.Net.Http.Headers;
+
+namespace ResultTracker.UI.Controllers
+{
+    public class TestResultsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public TestResultsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<TestResultDto> response = new List<TestResultDto>();
+            //Get all regions from web API: (HTTPclient class, way to consume web API's).
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                string token = HttpContext.User.FindFirst("TokenClaim").Value;
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7168/api/TestResults"); //Goes to host and then the url. 
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<TestResultDto>>());
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return View(response);
+        }
+    }
+}
---
+++ ResultTracker/ResultTracker.UI/Models/Dto/AccountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ResultTracker.UI.Models.Dto
+{
+    public class AccountDto
+    {
+        public string StudentName { get; set; } = string.Empty;
+        public string TeacherName { get; set; } = string.Empty;
+        public string StudentId { get; set; } = string.Empty;
+        public string TeacherId { get; set; } = string.Empty;
+    }
+}
