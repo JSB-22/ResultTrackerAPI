b5cc59e1cbcdb60083769c4005f9d2bbb1687390
JSB-22 <j.s.banyard@gmail.com>
2023-07-13 00:10:02 +0100

    Fixed validation problems

--- ResultTracker/ResultTracker.UI/Models/Dto/AddTestResultViewModel.cs
+++ ResultTracker/ResultTracker.UI/Models/Dto/AddTestResultViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
 
 namespace ResultTracker.UI.Models.Dto
@@ -7,17 +8,21 @@ namespace ResultTracker.UI.Models.Dto
     {
 
 		// For Drop down list functionality:
-		[Required(ErrorMessage = "A Topic must be assigned")]
 		public List<SelectListItem> TopicSelectList { get; set; }
-		[Required, Range(1, int.MaxValue, ErrorMessage = "Select a Topic")] // Range used so default option not usable. 
-		public string SelectedTopicId { get; set; } = string.Empty;
-		[Required(ErrorMessage = "A Subject must be assigned")]
 		public List<SelectListItem> SubjectSelectList { get; set; }
-		[Required, Range(1, int.MaxValue, ErrorMessage = "Select a Subject")]
-		public string SelectedSubjectId { get; set; } = string.Empty; //Convert back to GUID before post. 
-		[Required(ErrorMessage = "A Student must be assigned")]
 		public List<SelectListItem> AccountSelectList { get; set; }
-		[Required, Range(1, int.MaxValue, ErrorMessage = "Select a Student")]
+
+		// Fields the drop down lists pass to: (Note subject and topic need reconverting to GUID). 
+		[BindRequired]
+		[Required(ErrorMessage = "A Topic is required")]
+		public string SelectedTopicId { get; set; } = string.Empty;
+
+		[BindRequired]
+		[Required(ErrorMessage = "A Subject is required")]
+		public string SelectedSubjectId { get; set; } = string.Empty;
+
+		[BindRequired]
+		[Required(ErrorMessage = "A Student must be assigned")]
 		public string SelectedAccountId { get; set; } = string.Empty;
 
 		// Properties not requiring drop down: 
